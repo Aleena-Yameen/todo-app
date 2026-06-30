60478ff0124966ebf8e74cb7dbf092922638bb7c
Aleena-Yameen <aleenayameen50@gmail.com>
2026-06-30 17:02:23 +0500

    Added delete button feature

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ function addTask() {
     const input = document.getElementById("taskInput");
     const list = document.getElementById("taskList");
 
-    if (input.value === "") return;
+    if (input.value.trim() === "") {
+        return;
+    }
 
     const li = document.createElement("li");
-    li.textContent = input.value;
 
-    li.onclick = function () {
+    const taskText = document.createElement("span");
+    taskText.textContent = input.value;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function () {
         li.remove();
     };
 
+    li.appendChild(taskText);
+    li.appendChild(deleteBtn);
+
     list.appendChild(li);
+
     input.value = "";
 }
